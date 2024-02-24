1c32952d3032e0df49875e9f0843362f0d4cc88a
vvera2005 <vera.aleksanyan.05@gmail.com>
2024-02-24 13:50:56 +0400

    Some changes

--- MobileClasses/FlutterClasses/homeworks/lib/main.dart
+++ MobileClasses/FlutterClasses/homeworks/lib/main.dart
@@ -8,6 +8,7 @@ class Signin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Sign in"),
